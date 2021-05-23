78205967f03cf9110d106addb31d6e11eebe2ecf
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2021-05-23 18:23:44 +0700

    Added main.rs

---
+++ testing/src/main.rs
@@ -0,0 +1,51 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(testing::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use testing::{serial_print, serial_println};
+
+use cmos_rtc::{ReadRTC, Time};
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_print!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    testing::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
+
+#[test_case]
+fn test_cmos() {
+    let mut cmos = ReadRTC::new(0x00, 0x00);
+    let time: Time = cmos.read();
+    serial_println!("second: {}", time.minute);
+    serial_println!("minute: {}", time.minute);
+    serial_println!("hour: {}", time.hour);
+    serial_println!("day: {}", time.day);
+    serial_println!("month: {}", time.day);
+    serial_println!("year: {}", time.day);
+
+    assert_eq!(time.century, 0);
+}
